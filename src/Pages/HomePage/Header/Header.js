31b9bd0f830987c712a2ca8809db08f907b5dd49
aliftareq <aliftareq@gmail.com>
2023-02-02 09:00:35 +0600

    banner cmplt

--- src/Pages/HomePage/Header/Header.js
+++ src/Pages/HomePage/Header/Header.js
@@ -1,21 +1,28 @@
-import { Box, Container } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 import React from 'react';
-import bg from '../../../Assets/images/bannerbackground.png';
+
+import { HeroSectionWrapper, SearchBox } from './Header.style';
 
 const Header = () => {
     return (
-        <Box sx={{
-            height: '70vh',
-            width: '100%',
-            background: `url(${bg})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundPosition: 'center',
-            backgroundSize: '110%'
-        }}>
-            <Container>
-                <h1>Therer will be a banner for website</h1>
+        <HeroSectionWrapper>
+            <Container sx={{
+                height: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}>
+                <Box>
+                    <Typography variant='h3' fontWeight={'500'}>
+                        Best food waiting for your belly
+                    </Typography>
+                    <SearchBox>
+                        <input type="search" placeholder='search food items' />
+                        <button>Search</button>
+                    </SearchBox>
+                </Box>
             </Container>
-        </Box>
+        </HeroSectionWrapper>
     );
 };
 
